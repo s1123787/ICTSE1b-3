896cff8588ed4d3264cc5e5820d4f96cb3ec9ddd
Mick Nieweg (student) <s1123787@student.windesheim.nl>
2018-10-02 17:03:18 +0200

    Initial Solution commit
    Initial Solution commit + committing initial Window & GameCanvas

---
+++ KBSGame/KBSGame/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KBSGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            initStartpoint();
+            initEndpoint();
+        }
+
+        public void initStartpoint()
+        {
+            Ellipse cirkel = new Ellipse();
+            cirkel.Fill = Brushes.Gray;
+            cirkel.Width = 50;
+            cirkel.Height = 50;
+            Canvas.SetLeft(cirkel, 0);
+            Canvas.SetTop(cirkel, 0);
+            GameCanvas.Children.Add(cirkel);
+        }
+
+        public void initEndpoint()
+        {
+
+        }
+    }
+}
