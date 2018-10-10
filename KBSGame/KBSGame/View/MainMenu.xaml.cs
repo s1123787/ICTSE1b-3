d8f8ed4d85bfef5172c2358f1dec841a35f70afe
joeryoost <32954084+joeryoost@users.noreply.github.com>
2018-10-10 11:32:13 +0200

    mainmenu added to game

---
+++ KBSGame/KBSGame/View/MainMenu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KBSGame.View
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        MainWindow mainWindow = new MainWindow();
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void startGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void ExitGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
